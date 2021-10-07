0dd87ccbe848c251244d3371fb458710a2f24af9
ronibuchine <ronibuchine@gmail.com>
2021-10-07 18:28:02 +0300

    nothing

--- dotNet5782_0545_8228/Targil0/Program.cs
+++ dotNet5782_0545_8228/Targil0/Program.cs
@@ -6,7 +6,7 @@ namespace Targil0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
