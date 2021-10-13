9783acfc2d2c940802761cb3064166ebf6b40757
ronibuchine <ronibuchine@gmail.com>
2021-10-13 13:56:49 +0300

    added two welcome functions and renamed the program

---
+++ dotNet5782_0545_8228/Targil0/Program0545.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program0545
+    {
+        static void Main(string[] args)
+        {
+            Welcome0545();
+            Welcome8228();
+        }
+
+        private static void Welcome0545()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application!\n");
+        }
+        private static void Welcome8228()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application!\n");
+        }
+    }
+}
