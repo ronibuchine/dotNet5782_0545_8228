000124a7dec19206ef951a34cca9ee2a9a4a1cdc
ronibuchine <ronibuchine@gmail.com>
2021-10-18 13:55:28 +0300

    added enums

---
+++ dotNet5782_0545_8228/DAL/CodeFile1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        enum WeightCategories { heavy, medium, light };
+        enum DroneStatuses { free, maintenance, delivery };
+        enum Priorities { regular, fast, emergency };
+    }
+}
---
+++ dotNet5782_0545_8228/DAL/Drone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            public int id { get; set; }
+            public string model { get; set; }
+            double battery { get; set; }
+            WeightCategories maxWeight { get; set; }
+            DroneStatuses status { get; set; }
+            
+
+        }
+    }
+}
