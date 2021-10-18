825213ec96792514b03a365195b689589ac3fde2
Eli <elilevin11@gmail.com>
2021-10-18 20:20:17 +0300

    DALObject --started

--- dotNet5782_0545_8228/DAL/DataSource.cs
+++ dotNet5782_0545_8228/DAL/DataSource.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DalObject
 {
     class DataSource
     {
+        public static IDAL.DO.Drone[] Drones = new IDAL.DO.Drone[10];
+        public static IDAL.DO.DroneStation[] DroneStations = new IDAL.DO.DroneStation[5];
+        public static IDAL.DO.Customer[] Customers = new IDAL.DO.Customer[100];
+        public static IDAL.DO.Parcel[] Parcels = new IDAL.DO.Parcel[1000];
+
+
+        internal class Config
+        {
+            static int NextAvailableDroneIndex;
+            static int NextAvailableDroneStationIndex;
+            static int NextAvailableCustomerIndex;
+            static int NextAvailableParcelsIndex;
+            static int PackageCount;
+        }
+
+        public static void InitializeArrays()
+        {
+            // TODO: wtf goes here??
+        }
 
     }
 }
