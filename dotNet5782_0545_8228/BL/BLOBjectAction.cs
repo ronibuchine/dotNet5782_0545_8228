a73068a40b70ad1d03a486bd2e5846c10465e610
Eli <elilevin11@gmail.com>
2021-11-24 13:29:10 +0200

    some more BLOBjectActions

--- dotNet5782_0545_8228/BL/BLOBjectAction.cs
+++ dotNet5782_0545_8228/BL/BLOBjectAction.cs
@@ -31,9 +31,20 @@ namespace BLOBject
         public void ReleaseDroneFromCharge(int droneID, DateTime chargeTime)
         {
             IBL.BO.Drone drone = GetDrone(droneID);
-
+            if (drone.status != IBL.BO.DroneStatuses.maintenance) // is this always initialized?
+            {
+                throw new IBL.BO.InvalidBlObjectException("Drone is not currently in maintenance");
+            }
+            drone.battery = DateTime.UtcNow.Hour * chargingRate;
+            drone.status = IBL.BO.DroneStatuses.free;
+            int stationID = dal.GetAllCharges().Find((dc) => dc.DroneId == droneID).StationId;
+            dal.ReleaseDroneFromCharge(stationID, droneID);
         }
-        public void AssignPackageToDrone(int droneID);
+
+        public void AssignPackageToDrone(int droneID)
+        {
+            IBL.BO.Drone drone = GetDrone(droneID);
+        }
         public void CollectPackage(int droneID);
         public void DeliverPackage(int droneID);
 
