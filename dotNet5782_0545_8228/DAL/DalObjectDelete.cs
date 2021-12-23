1b0947eee97873f84dbc3865f4c918f2c2c3acf8
Eli <elilevin11@gmail.com>
2021-12-23 19:21:05 +0200

    delete correctly

--- dotNet5782_0545_8228/DAL/DalObjectDelete.cs
+++ dotNet5782_0545_8228/DAL/DalObjectDelete.cs
@@ -4,12 +4,12 @@ namespace DAL
 {
     public partial class DalObject : IDAL
     {
-        public void DeleteCustomer(int ID) => DataSource.customers.RemoveAll(c => c.ID == ID);
+        public void DeleteCustomer(int ID) => GetActualCustomer(ID).IsActive = false;
 
-        public void DeleteDrone(int ID) => DataSource.drones.RemoveAll(d => d.ID == ID);
+        public void DeleteDrone(int ID) => GetActualDrone(ID).IsActive = false;
 
-        public void DeletePackage(int ID) => DataSource.packages.RemoveAll(p => p.ID == ID);
+        public void DeletePackage(int ID) => GetActualPackage(ID).IsActive = false;
 
-        public void DeleteStation(int ID) => DataSource.stations.RemoveAll(s => s.ID == ID);
+        public void DeleteStation(int ID) => GetActualStation(ID).IsActive = false;
     }
 }
