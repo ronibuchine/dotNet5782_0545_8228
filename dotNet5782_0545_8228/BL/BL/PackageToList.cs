eb9fd8f0b8e46edb117e92ad1da956f363af687d
Eli <elilevin11@gmail.com>
2022-01-04 22:19:29 +0200

    PackageToList status

--- dotNet5782_0545_8228/BL/BL/PackageToList.cs
+++ dotNet5782_0545_8228/BL/BL/PackageToList.cs
@@ -24,14 +24,14 @@ namespace BL
             this.priority = package.priority;
 
             // there is a smarter way to do this. there must be.
-            if (package.delivered != null)
-                this.status = PackageStatuses.delivered;
-            if (package.pickedUp != null)
-                this.status = PackageStatuses.pickedUp;
-            if (package.scheduled != null)
-                this.status = PackageStatuses.scheduled;
             if (package.requested != null)
                 this.status = PackageStatuses.requested;
+            if (package.scheduled != null)
+                this.status = PackageStatuses.scheduled;
+            if (package.pickedUp != null)
+                this.status = PackageStatuses.pickedUp;
+            if (package.delivered != null)
+                this.status = PackageStatuses.delivered;
         }
 
         public override string ToString()
